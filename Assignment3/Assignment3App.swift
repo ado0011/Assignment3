a4751dbaff89d670939b7235e6b5ff477dce30f4
ado0011 <ado0011@auburn.edu>
2023-11-05 18:18:58 -0800

    Initial Commit

---
+++ Assignment3/Assignment3App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment3App.swift
+//  Assignment3
+//
+//  Created by user247159 on 11/5/23.
+//
+
+import SwiftUI
+
+@main
+struct Assignment3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
